fdf410c5ae6c75fc5bc31588bdd0517e123333d3
BOSS-PC\boss <contact@eplex.eu>
2017-11-16 12:27:32 +0100

    app to modules + webpack

--- src/app.js
+++ src/app.js
@@ -1,27 +1,5 @@
-//import './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp.js';
 
-import substract, {square, add} from './utils.js';
-
-console.log('app.js is running!!');
-
-console.log(square(4));
-console.log(add(100,100));
-
-console.log(substract(100, 79));
-
-console.log('----------------');
-
-import isSenior, {isAdult, canDrink} from './person.js';
-
-console.log(isAdult(16));
-console.log(isAdult(19));
-
-console.log(canDrink(20));
-console.log(canDrink(24));
-console.log(isSenior(64));
-console.log(isSenior(65));
-
-console.log('----------------');
-import validator from 'validator';
-
-console.log(validator.isEmail('test@22.pl'));
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
---
+++ src/components/AddOption.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class AddOption extends React.Component {
+    constructor(props) {
+        super(props);
+        this.AddOption = this.AddOption.bind(this);
+        this.state = {
+            error: undefined
+        };
+    }
+    AddOption(e) {
+        e.preventDefault();
+        const option = e.target.elements.option.value.trim();
+
+        const error = this.props.handleAddOption(option);
+        this.setState(() => ({error: error}));
+
+        if (!error) {
+            e.target.elements.option.value = '';
+        }
+    }
+    render() {
+        return (
+            <div>
+                {this.state.error && <p>{this.state.error}</p>}
+                <form onSubmit={this.AddOption}>
+                    <input type="text" name="option" />
+                    <button name="btnAdd">Add option</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddOption;
