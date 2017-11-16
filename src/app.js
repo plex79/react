549911d6724732937ee3d95884b98d8f20fc815b
BOSS-PC\boss <contact@eplex.eu>
2017-11-16 10:18:59 +0100

    webpack import export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,22 @@
+//import './utils.js';
+
+import substract, {square, add} from './utils.js';
+
 console.log('app.js is running!!');
+
+console.log(square(4));
+console.log(add(100,100));
+
+console.log(substract(100, 79));
+
+console.log('----------------');
+
+import isSenior, {isAdult, canDrink} from './person.js';
+
+console.log(isAdult(16));
+console.log(isAdult(19));
+
+console.log(canDrink(20));
+console.log(canDrink(24));
+console.log(isSenior(64));
+console.log(isSenior(65));
---
+++ src/person.js
@@ -0,0 +1,7 @@
+
+export const isAdult = (age) => age >= 18 ? true : false;
+
+export const canDrink = (age) => age >= 21 ? 'moze pic' : 'nie moze pic';
+
+const isSenior = (age) => age >=65;
+export default isSenior;
