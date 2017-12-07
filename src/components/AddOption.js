e14008d7588174b6de36e60f280a43d25191f695
BOSS-PC\boss <contact@eplex.eu>
2017-12-07 12:03:16 +0100

    app end styling!

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -19,9 +19,9 @@ class AddOption extends React.Component {
     render() {
         return (
             <div>
-                {this.state.error && <p>{this.state.error}</p>}
-                <form onSubmit={this.AddOption}>
-                    <input type="text" name="option" />
+                {this.state.error && <p className="add-option-error">{this.state.error}</p>}
+                <form className="add-option" onSubmit={this.AddOption}>
+                    <input className="add-option__input" type="text" name="option" />
                     <button className="button" name="btnAdd">Add option</button>
                 </form>
             </div>
